1ca58a90ab4adde48f53f1f8c8db1dafe48edced
JoshuaFrezy <32793563+JoshuaFrezy@users.noreply.github.com>
2017-10-31 10:08:55 +0700

    Update bro.py

--- bro.py
+++ bro.py
@@ -80,7 +80,7 @@ Bmid = kk.getProfile().mid
 Cmid = kc.getProfile().mid
 
 Bots=[mid,Amid,Bmid,Cmid]
-admin=["YOUR_MID_HERE"]
+admin=["uc77fd25b59f6e563d84f1334f3fed10b","u2b457b1ef8391ee451c9a22c835e1e33"]
 wait = {
     'contact':True,
     'autoJoin':True,
